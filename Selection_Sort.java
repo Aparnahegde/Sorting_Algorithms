a6226ef68dd0323067feadc1577f2f4be924a8c1
Aparnahegde <92423253+Aparnahegde@users.noreply.github.com>
2024-05-23 09:39:48 +0530

    Update Selection_Sort.java

--- Selection_Sort.java
+++ Selection_Sort.java
@@ -1,10 +1,6 @@
+import java.util.Arrays;
 public class Main {
-    public static void printArray(int[] arr) {
-        for (int i=0;i<arr.length;i++) {
-            System.out.print(arr[i]+" ");
-        }
-    }
-    public static void main(String[] args) {
+  public static void main(String[] args) {
         int[] arr = {4, 2, 7, 1};
         for (int i = 0; i < arr.length-1; i++) {
             int smallest = i;
@@ -17,7 +13,7 @@ public class Main {
             arr[smallest] = arr[i];
             arr[i] = temp;
         }
-        printArray(arr);
+         System.out.println(Arrays.toString(arr));
     }
 }
 
